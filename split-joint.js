2a150066f128f580e4e34f752e1c47b11999fe6c
unknown <mehedi15-2995@diu.edu.bd>
2022-08-07 09:19:11 +0600

    another

--- split-joint.js
+++ split-joint.js
@@ -1,2 +1,25 @@
 const lyrics = 'tumi bndu kala pakhi . ami jeno ki .boshonto kale tumay bolte parini . kala kala shada shada'
-console.log(lyrics);
+    //devide into words
+    const parts = lyrics.split(' ');
+
+    //devide into lines
+    const sentence = lyrics.split('.');
+
+    //devide every character
+    const char =lyrics.split('')
+
+//console.log(char);
+
+const partial = lyrics.slice(5,8);
+//console.log(partial);
+const partial2 = lyrics.substring(5,8);
+console.log(partial2);
+
+const newLine =[
+    'tumi bndu kala pakhi ',
+    'ami jeno ki ',
+    'boshonto kale tumay bolte parini ',
+    'kala kala shada shada'
+  ]
+  const newSong = newLine.join('; ')
+  console.log(newSong);
