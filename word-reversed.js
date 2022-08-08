69c7fdd6d70c8d70f77e929b4a00f55f1d53488b
unknown <mehedi15-2995@diu.edu.bd>
2022-08-08 13:46:07 +0600

    added

---
+++ word-reversed.js
@@ -0,0 +1,18 @@
+//word-reversed.js
+
+function reverseWord(str){
+    const words = str.split(' ');
+    const result = [];
+    // [ 'I', 'am', 'a', 'good', 'boy.' ]
+    for(let i =words.length-1; i>=0;i--){
+            const element = words[i];
+            result.push(element)
+    }
+    //console.log(result);
+    const reversed = result.join(' ');
+    return reversed;
+}
+
+
+const myString = 'I am a good boy.'
+reverseWord(myString);
